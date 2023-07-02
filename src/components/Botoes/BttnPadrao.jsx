8558b66f5641e5a3667947f2befc450dd214ab2f
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-07-02 18:05:26 -0300

    botão padrão

---
+++ src/components/Botoes/BttnPadrao.jsx
@@ -0,0 +1,12 @@
+export default function BttnPadrao({ texto, onClick }) {
+	return (
+		<>
+			<button
+				className="text-sm col-span-2 h-10 bg-element-blue text-white rounded-xl cursor-pointer hover:bg-accent-orange"
+				onClick={onClick}
+			>
+				{texto}
+			</button>
+		</>
+	);
+}
