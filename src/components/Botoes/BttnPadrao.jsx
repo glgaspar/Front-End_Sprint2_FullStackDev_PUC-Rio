55c24aceefbe239e6999d565876928ded0c2a728
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-07-07 16:24:25 -0300

    padding no botao

--- src/components/Botoes/BttnPadrao.jsx
+++ src/components/Botoes/BttnPadrao.jsx
@@ -2,7 +2,7 @@ export default function BttnPadrao({ texto, onClick }) {
 	return (
 		<>
 			<button
-				className="text-sm col-span-2 h-10 bg-element-blue text-white rounded-xl cursor-pointer hover:bg-accent-orange"
+				className="text-sm col-span-2 h-10 p-2 bg-element-blue text-white rounded-xl cursor-pointer hover:bg-accent-orange"
 				onClick={onClick}
 			>
 				{texto}
