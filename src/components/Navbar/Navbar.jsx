4cc4a74d8702cd2f556cc3b8de8b9eb31aec095c
Gustavo Gaspar <glgaspar@live.com>
2023-07-11 22:57:45 -0300

    corringindo  seta de retorno

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,32 @@
 import React from "react";
 import { IconContext } from "react-icons";
-import * as Io from "react-icons/io5"
+import * as Io from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
-import logo from "./logo192.png"
+import logo from "./logo192.png";
 function Navbar() {
-
-	const pathnname  = window.location.pathname
-
-	const navigate = useNavigate()
+	const pathname = window.location.pathname;
+	const navigate = useNavigate();
 	
 	return (
-		<>
-			<IconContext.Provider value={{ color: "#ffffff" }}>
-				<div className="grid grid-cols-9 bg-main-blue">
-					<div className="p-1 col-span-2">
-						<div className="text-[2rem] cursor-pointer">
-							{pathnname !== '/home' && <Io.IoArrowBackSharp onClick={()=>navigate('/home')} />}
-						</div>
-					</div>
-					<div className="col-span-2"></div>
-					<div className="col-span-1 ml-auto mr-auto">
-						<Link to="/home">
-							<img src={logo} className="h-[6vh]" alt="logo" />
-						</Link>
-					</div>
-					
+    <>
+		<IconContext.Provider value={{ color: "#ffffff" }}>
+			<div className="grid grid-cols-9 bg-main-blue">
+			<div className="p-1 col-span-2">
+				<div className="text-[2rem] cursor-pointer">
+				{pathname !== "/" && pathname !== "/home" && (
+					<Io.IoArrowBackSharp onClick={() => navigate("/home")} />
+				)}
 				</div>
-			</IconContext.Provider>
-		</>
+			</div>
+			<div className="col-span-2"></div>
+			<div className="col-span-1 ml-auto mr-auto">
+				<Link to="/home">
+				<img src={logo} className="h-[6vh]" alt="logo" />
+				</Link>
+			</div>
+			</div>
+		</IconContext.Provider>
+	</>
 	);
 }
 
