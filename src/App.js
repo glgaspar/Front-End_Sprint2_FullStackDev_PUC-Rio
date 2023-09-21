42b1cc3f04d25dbc6c1561dc163f8fb14a317dd0
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-09-20 21:34:13 -0300

    controle de login para rotas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
 import NewOrder from "./pages/NewOrder";
 import OrderDetail from "./pages/OrderDetail";
+import Login from "./components/Login/Login";
 
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
+  
+  const Private = ({Item}) => {
+    const signed = JSON.parse(sessionStorage.getItem('user'));
+
+    return signed !== null ? Item  : <Login /> 
+}
+
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/home" element={<Home />} exact />
-        <Route path="/detalhe" element={<OrderDetail />} exact />
-        <Route path="/novo" element={<NewOrder />} exact />
+        <Route path="/" element={<Private Item={<Home />} />} exact />
+        <Route path="/home" element={<Private Item={<Home />} />} exact />
+        <Route path="/detalhe" element={<Private Item={<OrderDetail />} />} exact />
+        <Route path="/novo" element={<Private Item={<NewOrder />} />} exact />
       </Routes>
     </Router>
   )
