4f142c65eb2f41d11a6ca0d2a95de33f59705133
glgaspar <77644677+glgaspar@users.noreply.github.com>
2023-09-20 21:29:59 -0300

    botao com caixa de dialogo

---
+++ src/components/Botoes/BttnDialog.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+export default function BttnDialog(props) {
+	const [aberto, setAberto] = useState(false);
+
+	const handleBttn = () => {
+		props.onClick();
+		setAberto(!aberto);
+	};
+
+
+	return (
+		<div>
+			<span  onClick={() => setAberto(!aberto)}>{props.texto}</span>
+			<dialog
+				open={aberto}
+				className="fixed m-auto border rounded-xl bg-component-whitesmoke hover:cursor-default shadow-md"
+			>
+				<h1 className="mt-5">{props.mensagem}</h1>
+					<div className="grid grid-flow-col gird-cols-2 gap-5 m-5">
+                    <button
+							className="border rounded-xl p-1 text-xs bg-element-blue text-white w-25 hover:bg-accent-orange hovevr:curso-pointer"
+                            onClick={()=>setAberto(!aberto)}
+						>
+							Cancelar
+						</button>
+						<button
+							className="border rounded-xl p-1 text-xs bg-element-blue text-white w-25 hover:bg-accent-orange hovevr:curso-pointer"
+							onClick={handleBttn}
+						>
+							Confirmar
+						</button>
+						
+					</div>
+			</dialog>
+		</div>
+	);
+}
